390a429068788d267dd7638f6b687506a53557c2
Eriv <38401280+Eric@users.noreply.github.com>
2020-02-02 20:31:28 +0100

    Got Some shit Done

---
+++ src/main/dataTransfer/Solve.java
@@ -0,0 +1,39 @@
+package main.dataTransfer;
+
+import java.net.Socket;
+
+public class Solve {
+    private String puzzle;
+    private String specializations;
+    private String solveTime;
+    private String date;
+    private String scramble;
+
+    public Solve(String puzzle, String specializations, String solveTime, String date,String scramble){
+        this.puzzle = puzzle;
+        this.specializations = specializations;
+        this.solveTime = solveTime;
+        this.date = date;
+        this.scramble = scramble;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPuzzle() {
+        return puzzle;
+    }
+
+    public String getScramble() {
+        return scramble;
+    }
+
+    public String getSolveTime() {
+        return solveTime;
+    }
+
+    public String getSpecializations() {
+        return specializations;
+    }
+}
--- src/main/tests/CSVReader.java
+++ src/main/tests/CSVReader.java
@@ -1,12 +1,13 @@
 package main.tests;
 
 
+import main.dataTransfer.Solve;
+
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CSVReader {
-    private BufferedReader reader;
-    private String path;
-    private PrintWriter writer;
 
     public void CSVReader() {
         //TODO
@@ -14,8 +15,8 @@ public class CSVReader {
 
     public void write(String input, String path) {
         try {
-            writer = new PrintWriter(new File(path));
-            writer.write(input+"\n");
+            FileWriter writer = new FileWriter(new File(path), true);
+            writer.append(input).append("\n");
             writer.flush();
             writer.close();
         } catch (IOException e) {
@@ -23,13 +24,39 @@ public class CSVReader {
         }
     }
 
+    public List<String> read(String path){
+        List<String> ret = new LinkedList<>();
+        String line = "";
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            while((line = reader.readLine()) != null){
+                ret.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
 
+    public List<Solve> readSolveForm(String path){
+        List<Solve> ret = new LinkedList<>();
+        String line = "";
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            while((line = reader.readLine()) != null){
+                String[] arr = line.split(",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
     public static void main(String[] args) {
         String csvFile = "data/testData.csv";
         CSVReader reader = new CSVReader();
-        reader.write("Hello",csvFile);
-        reader.write("Hello",csvFile);
-        reader.write("Hello",csvFile);
+        List<String> list = reader.read(csvFile);
+        for(String s : list)
+            System.out.println(s);
     }
 
 }
