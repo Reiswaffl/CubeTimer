c33c1aa90d27a9a2619370355c0a27a9a34c9765
Eriv <38401280+Eric@users.noreply.github.com>
2020-02-06 21:53:27 +0100

    Got Some shit Done

--- src/main/GUI/MainWindowController.java
+++ src/main/GUI/MainWindowController.java
@@ -1,11 +1,31 @@
 package main.GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainWindowController {
 
     public GUI gui;
-
+    @FXML  Label solvingTime;
     // Methods
     public void setGui(GUI gui) {
         this.gui = gui;
+    }
+
+    @FXML
+    public void initialize(){
+        Refresher f = new Refresher(solvingTime);
+        f.start();
+    }
+
+    static class Refresher extends Thread{
+        Label l;
+        public Refresher(Label l){
+            this.l = l;
+        }
+        @Override
+        public void run(){
+
+        }
     }
 }
